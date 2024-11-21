7805c46ed538c5336ba4c233d160b9d6911f8c50
Han <1655466387@qq.com>
2024-11-21 17:59:28 +0800

    添加打赏图片✅

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -10,7 +10,7 @@
       </section>
       <section class="flex flex-col items-center gap-2">
         <div class="flex gap-6">
-          <a title="打赏站长" target="_blank" href="@/assets/images/reward.webp">
+          <a title="打赏站长" target="_blank" href="/reward.webp">
             <img class="w-8" src="@/assets/images/footer/shang.svg" />
           </a>
           <a title="Github" target="_blank" rel="nofollow" href="https://github.com/uxiaohan">
